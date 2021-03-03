37a479d9825775911cc248501b6098756ca6f9cc
zaraahlie <71647345+zaraahlie@users.noreply.github.com>
2021-03-02 19:25:32 -0500

    Got the server working! Also added pug package through NPM.
    When running the server you have to npm install inside the github directory through terminal.

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const http = require("http");
+const pug = require("pug");
 let app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+//Serve static resources from public, if they exist
+app.use(express.static("static_pgs"));
 
 //app.use = for ANY request
 //We will see ways to specify route-specific handlers soon
@@ -10,10 +18,46 @@ app.use(function(req,res,next){
 	next();
 });
 
-//Serve static resources from public, if they exist
-app.use(express.static("static_pgs"));
-
 //If the resource wasn't in other, continue the chain
+app.get(['/', '/profile'], (req, res) => {
+	res.render('./primaries/userprofile', {})
+})
+
+app.get('/profile', (req, res) => {
+	res.render('./primaries/userprofile', {})
+})
+
+app.get('/addmovie', (req, res) => {
+	res.render('./primaries/addamovie', {})
+})
+
+app.get('/addperson', (req, res) => {
+	res.render('./primaries/addaperson', {})
+})
+
+app.get('/logout', (req, res) => {
+	res.redirect('signin.html');
+})
+
+app.get('/signin', (req, res) => {
+	res.redirect('signin.html');
+})
+
+app.get('/signup', (req, res) => {
+	res.redirect('signup.html');
+})
+
+app.get('/viewuser', (req, res) => {
+	res.render('./primaries/viewingusers', {})
+})
+
+app.get('/viewpeople', (req, res) => {
+	res.render('./primaries/viewingpeople', {})
+})
+
+app.get('/movieprofile', (req, res) => {
+	res.render('./primaries/movieprofile', {})
+})
 
 //This is a shorthand way of creating/initializing the HTTP server
 app.listen(3000);
