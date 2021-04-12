97e75d5a4647aeefd3378bc64264579ae8c7780a
zaraahlie <71647345+zaraahlie@users.noreply.github.com>
2021-04-12 12:06:15 -0400

    A lot of AddMovie progress
    closes #32, progress on #47

--- public/javascript/addmovie.js
+++ public/javascript/addmovie.js
@@ -1,17 +1,13 @@
 let directors=[];
 let writers=[];
 let actors=[];
+let genres = [];
 
-function verifynumber(year){
-  if (typeof year == 'number'){
-    return true;
-  } //checks if it's a number
-  return false;
+function verifynumber(number){
+  console.log(!isNaN(parseInt(number)));
+  return(!isNaN(parseInt(number)));
 }
 
-function movieIsInServer(){
-  return false;
-}
 
 function getDirectors(){
   let input = document.getElementById("directors").value;
@@ -68,6 +64,46 @@ function display(result, path){
   (result.names).forEach(name => names+= "<option value='"+name.name + "'/>");
   div.innerHTML = names;
 }
+
+function addWriters(){
+  let input = document.getElementById("writers").value.trim();
+  let datalist =  document.getElementById("writerNames");
+  let div = document.getElementById("writ");
+
+  addNames(input, datalist, div, writers);
+}
+
+function addNames(input, datalist, div, array){
+  for(let i = 0; i < datalist.options.length; i++){
+    if(input === datalist.options[i].value){
+      if(!isDuplicate(datalist.options[i].value, array)){
+        array.push(datalist.options[i].value);
+        let names = div.innerHTML;
+        if(names === undefined){
+          names = "";
+        }
+        names += "<p>"+datalist.options[i].value+"</p>";
+        console.log(names);
+        div.innerHTML = names;
+        console.log(div);
+      }
+    }
+  }
+}
+
+function addDirectors(){
+  let input = document.getElementById("directors").value.trim();
+  let datalist =  document.getElementById("directorNames");
+  let div = document.getElementById("direct");
+  addNames(input, datalist, div, directors);
+}
+
+function addActors(){
+  let input = document.getElementById("actors").value.trim();
+  let datalist =  document.getElementById("actorNames");
+  let div = document.getElementById("act");
+  addNames(input, datalist, div, actors);
+}
 //The page must give the user a way to dynamically search for people within the
 //database to add to the movie (e.g., using AJAX).
 //The user should not be required to type in the full name of the person,
@@ -76,30 +112,87 @@ function display(result, path){
 
 //ADD EVENT LISTENERS FOR THE WRITERS, DIRECTORS, ACTORS BOXES SO THE SEARCH RESULTS AUTO UPDATE
 
-function addMovie(){
-  let title = document.getElementById("title").value;
-  let year = document.getElementById("year").value;
-  let runtime = document.getElementById("runtime").value;
-  let genres = document.getElementById("genres").value;
-  let writers = document.getElementById("writers").value;
-  let directors = document.getElementById("directors").value;
-  let actors = document.getElementById("actors").value;
+function addGenres(){
+  let input = document.getElementById("genres").value.trim();
+  if(!isDuplicate(input, genres)){
+    genres.push(input);
+    let div = document.getElementById("gen");
+    let gen = div.innerHTML;
+    if(gen === undefined){
+      gen = "";
+    }
+    gen += "<p>"+input+"</p>";
+    console.log(gen);
+    div.innerHTML = gen;
+    console.log(div);
+  }
+}
 
-	if(title.length == 0 || year.length == 0 || runtime.length == 0 || genres.length == 0){
+function isDuplicate(name, array){
+  return array.includes(name);
+}
+
+function addMovie(){
+  let title = document.getElementById("title").value.trim();
+  let year = document.getElementById("year").value.trim();
+  let runtime = document.getElementById("runtime").value.trim();
+
+  if((!verifynumber(year)|| !verifynumber(runtime)) && genres.length > 0 && title.length > 0){
+    console.log(year);
+    console.log(runtime);
+    alert("Please make sure you are entering your answers in numerical format for the year and release year.");
+  }
+
+	else if(title.length == 0 || year.length == 0 || runtime.length == 0 || genres.length == 0){
 		alert("All fields are required to add a movie.");
 		return;
 	}
-  else if(!verifynumber(releaseyear)|| !verifynumber(runtime)){
-    alert("Please make sure you are entering your answers in numerical format when a number is required.")
+  else if(directors.length < 0 || actors.length < 0 || writers.length < 0){
+    alert("At least one director, writer and actor is needed to add a movie to the database.");
   }
-
-	if(!movieIsInServer()){ //checks server to see if the movie is in the server or not
-    genres = genres.split(",");//splits genres by comma
-    writers = writers.split(",");//most likely need to trim off whitespace
-    directors = directors.split(",");
-    actors = actors.split(",");
+  else if(genres.length < 0){
+    alert("At least one genre must be added.");
+  }
+  else{
+    createObject(title, year, runtime);
+  }
     //now have array of each.
-    let newMovie = {"Title": title, "Year":releaseyear, "Runtime": runtime + " min", "Genre":genres, "Director": directors, "Writer": writers, "Actors": actors};
+    //let newMovie = {"Title": title, "Year":releaseyear, "Runtime": runtime + " min", "Genre":genres, "Director": directors, "Writer": writers, "Actors": actors};
     //send movie to server
-  }
+  //}
+}
+
+function createObject(title, year, runtime){
+  let movie = {};
+  movie.title = title;
+  movie.year = year;
+  movie.runtime = runtime;
+  movie.genres = genres;
+  movie.director = directors;
+  movie.actor = actors;
+  movie.writer = writers;
+}
+
+function sendServerRequest(){
+    let xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+  		if(this.readyState==4){
+        if(this.status==201){
+          let id = (JSON.parse(this.responseText))._id;
+          window.location.replace(`/people/${id}`);
+        }
+        else if(this.status==401){
+          alert("You are not authorized to add a person to the database.");
+        }
+        else if(this.status==409){
+          alert("This person already exists in the database. You can search for them in the search tab.");
+        }
+        else{
+          alert("There was a problem with the server. Try again.");
+        }
+  		}
+  	};
+  	xhttp.open("POST", "/addperson", true);
+  	xhttp.setRequestHeader("Content-Type", "application/json");
+  	xhttp.send(JSON.stringify({"name": name}));
 }
